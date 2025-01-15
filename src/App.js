12e884109caf2828c39bf0c954b9cc1dd0cbb988
hang-on <hang-on@users.noreply.github.com>
2025-01-15 20:12:53 +0100

    Trim away redundant midi event listener

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ import { onMIDIMessage, initializeMIDI } from './utils/index.js';
 const App = () => {
     document.addEventListener('DOMContentLoaded', () => {
         initializeMIDI();
-
-        // Set up MIDI message handler
-        if (navigator.requestMIDIAccess) {
-            navigator.requestMIDIAccess().then((midiAccess) => {
-                midiAccess.inputs.forEach((input) => {
-                    input.onmidimessage = onMIDIMessage;
-                });
-            });
-        } else {
-            console.warn('WebMIDI is not supported in this browser.');
-        }
     });
 };
 
