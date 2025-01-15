f46d134b1104d04c8bbcbf850dca17a09ef40944
hang-on <hang-on@users.noreply.github.com>
2025-01-15 17:57:05 +0100

    Refactor midi even more

--- src/utils/midi.js
+++ src/utils/midi.js
@@ -1,30 +1,54 @@
-const scientificPitchNotationToMIDI = {
-    'C0': 12, 'C#0': 13, 'D0': 14, 'D#0': 15, 'E0': 16, 'F0': 17, 'F#0': 18, 'G0': 19, 'G#0': 20, 'A0': 21, 'A#0': 22, 'B0': 23,
-    'C1': 24, 'C#1': 25, 'D1': 26, 'D#1': 27, 'E1': 28, 'F1': 29, 'F#1': 30, 'G1': 31, 'G#1': 32, 'A1': 33, 'A#1': 34, 'B1': 35,
-    'C2': 36, 'C#2': 37, 'D2': 38, 'D#2': 39, 'E2': 40, 'F2': 41, 'F#2': 42, 'G2': 43, 'G#2': 44, 'A2': 45, 'A#2': 46, 'B2': 47,
-    'C3': 48, 'C#3': 49, 'D3': 50, 'D#3': 51, 'E3': 52, 'F3': 53, 'F#3': 54, 'G3': 55, 'G#3': 56, 'A3': 57, 'A#3': 58, 'B3': 59,
-    'C4': 60, 'C#4': 61, 'D4': 62, 'D#4': 63, 'E4': 64, 'F4': 65, 'F#4': 66, 'G4': 67, 'G#4': 68, 'A4': 69, 'A#4': 70, 'B4': 71,
-    'C5': 72, 'C#5': 73, 'D5': 74, 'D#5': 75, 'E5': 76, 'F5': 77, 'F#5': 78, 'G5': 79, 'G#5': 80, 'A5': 81, 'A#5': 82, 'B5': 83,
-    'C6': 84, 'C#6': 85, 'D6': 86, 'D#6': 87, 'E6': 88, 'F6': 89, 'F#6': 90, 'G6': 91, 'G#6': 92, 'A6': 93, 'A#6': 94, 'B6': 95,
-    'C7': 96, 'C#7': 97, 'D7': 98, 'D#7': 99, 'E7': 100, 'F7': 101, 'F#7': 102, 'G7': 103, 'G#7': 104, 'A7': 105, 'A#7': 106, 'B7': 107,
-    'C8': 108
-};
-
-const MIDIToScientificPitchNotation = {};
-for (let note in scientificPitchNotationToMIDI) {
-    MIDIToScientificPitchNotation[scientificPitchNotationToMIDI[note]] = note;
-}
-
+// Constants for MIDI message types
 const NOTE_ON = 144;
 const NOTE_OFF = 128;
 
+// Set to keep track of active notes
 let activeNotes = new Set();
 
 // Global variable to control logging
-const isLoggingEnabled = false;  
+const isLoggingEnabled = false;
 
-export function getActiveNotes(){
-    return activeNotes
+const scientificPitchNotationToMIDI = {
+    'c0': 12, 'c#0': 13, 'd0': 14, 'd#0': 15, 'e0': 16, 'f0': 17, 'f#0': 18, 'g0': 19, 'g#0': 20, 'a0': 21, 'a#0': 22, 'b0': 23,
+    'c1': 24, 'c#1': 25, 'd1': 26, 'd#1': 27, 'e1': 28, 'f1': 29, 'f#1': 30, 'g1': 31, 'g#1': 32, 'a1': 33, 'a#1': 34, 'b1': 35,
+    'c2': 36, 'c#2': 37, 'd2': 38, 'd#2': 39, 'e2': 40, 'f2': 41, 'f#2': 42, 'g2': 43, 'g#2': 44, 'a2': 45, 'a#2': 46, 'b2': 47,
+    'c3': 48, 'c#3': 49, 'd3': 50, 'd#3': 51, 'e3': 52, 'f3': 53, 'f#3': 54, 'g3': 55, 'g#3': 56, 'a3': 57, 'a#3': 58, 'b3': 59,
+    'c4': 60, 'c#4': 61, 'd4': 62, 'd#4': 63, 'e4': 64, 'f4': 65, 'f#4': 66, 'g4': 67, 'g#4': 68, 'a4': 69, 'a#4': 70, 'b4': 71,
+    'c5': 72, 'c#5': 73, 'd5': 74, 'd#5': 75, 'e5': 76, 'f5': 77, 'f#5': 78, 'g5': 79, 'g#5': 80, 'a5': 81, 'a#5': 82, 'b5': 83,
+    'c6': 84, 'c#6': 85, 'd6': 86, 'd#6': 87, 'e6': 88, 'f6': 89, 'f#6': 90, 'g6': 91, 'g#6': 92, 'a6': 93, 'a#6': 94, 'b6': 95,
+    'c7': 96, 'c#7': 97, 'd7': 98, 'd#7': 99, 'e7': 100, 'f7': 101, 'f#7': 102, 'g7': 103, 'g#7': 104, 'a7': 105, 'a#7': 106, 'b7': 107,
+    'c8': 108
+};
+
+/**
+ * Converts a scientific pitch notation note to its corresponding MIDI value.
+ * @param {string} note - The scientific pitch notation note (e.g., 'c4').
+ * @returns {number} The corresponding MIDI value, or undefined if the note is not found.
+ */
+export function getMIDINoteValue(note) {
+    return scientificPitchNotationToMIDI[note.toLowerCase()];
+}
+
+/**
+ * Converts a MIDI note value to its corresponding scientific pitch notation.
+ * @param {number} midiValue - The MIDI note value (e.g., 60).
+ * @returns {string} The corresponding scientific pitch notation, or undefined if the value is not found.
+ */
+export function getScientificPitchNotation(midiValue) {
+    const MIDIToScientificPitchNotation = {};
+    for (let note in scientificPitchNotationToMIDI) {
+        MIDIToScientificPitchNotation[scientificPitchNotationToMIDI[note]] = note;
+    }
+    return MIDIToScientificPitchNotation[midiValue];
+}
+
+/**
+ * Retrieves the currently active MIDI notes.
+ *
+ * @returns {Set} A set of active MIDI notes.
+ */
+export function getActiveNotes() {
+    return activeNotes;
 }
 
 // Function to initialize MIDI access
@@ -63,29 +87,29 @@ export function onMIDIMessage(event) {
     // MIDI message structure: [command, midiNote, velocity]
     const [command, midiNote, velocity] = event.data;
     // Scientific Pitch Notation (spr)
-    const sprNote = MIDIToScientificPitchNotation[midiNote];
+    const note = getScientificPitchNotation(midiNote);
 
-    if (sprNote === undefined) {
-        if (isLoggingEnabled) console.log(`Note ${midiNote} is not mapped to any note on the midi-to-note map.`);
+    if (note === undefined) {
+        if (isLoggingEnabled) console.log(`Note ${midiNote} is not mapped to any note.`);
         return;
     }
 
     if (command === NOTE_ON && velocity > 0) { 
-        if (!activeNotes.has(sprNote)) {
-            activeNotes.add(sprNote);
-            if (isLoggingEnabled) console.log(`Note on: ${sprNote} (velocity: ${velocity})`);
+        if (!activeNotes.has(note)) {
+            activeNotes.add(note);
+            if (isLoggingEnabled) console.log(`Note on: ${note} (velocity: ${velocity})`);
         }
     } else if (command === NOTE_OFF || (command === NOTE_ON && velocity === 0)) {
-        if (activeNotes.has(sprNote)) {
-            activeNotes.delete(sprNote);
-            if (isLoggingEnabled) console.log(`Note off: ${sprNote}`);
+        if (activeNotes.has(note)) {
+            activeNotes.delete(note);
+            if (isLoggingEnabled) console.log(`Note off: ${note}`);
         }
     }
 
     if (isLoggingEnabled) {
         console.log(`MIDI message received: ${event.data}`);
         console.log(`Command: ${command}, Note: ${midiNote}, Velocity: ${velocity}`);
-        console.log(`Played Note: ${sprNote}`);
+        console.log(`Played Note: ${note}`);
         console.log(`Active Notes: ${Array.from(activeNotes).join(', ')}`);
     }
 }
