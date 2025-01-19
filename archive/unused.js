8967b960a4fdd6ddc5fd939e9d36cc66c34f1d83
hang-on <hang-on@users.noreply.github.com>
2025-01-19 08:42:36 +0100

    Purge archive

--- archive/unused.js
+++
@@ -1,9 +0,0 @@
-/**
- * Renders all notes within the container.
- * @param {HTMLElement} container - The container to render the notes in.
- */
-function renderNotes(container) {
-    notesData.forEach((noteData, index) => {
-        addNoteAtPosition(container, index * 50, noteData.y, noteData.strikeThrough);
-    });
-}
