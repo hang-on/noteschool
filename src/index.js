c13e002c1308e46a575110a64e9490500ce39b77
hang-on <hang-on@users.noreply.github.com>
2025-02-01 19:19:33 +0100

    Add MIDI info section and initialize MIDI on window load

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { stats, saveStats} from './stats.js';
+import { initializeMIDI } from './utils/index.js';
+
+window.onload = function(){
+    initializeMIDI();
+};
 
 document.getElementById('start-button').addEventListener('click', function() {
     // Reset stats in localStorage
