cb2331a48418985caa39d1990e4b0f9e32c15da4
hang-on <hang-on@users.noreply.github.com>
2025-02-01 19:30:05 +0100

    Add comment to clarify MIDI device listing in window onload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { stats, saveStats} from './stats.js';
 import { initializeMIDI } from './utils/index.js';
 
 window.onload = function(){
+    // List available midi-devices in the designated section.
     initializeMIDI(dummy(), true);
 };
 
