c8e42bf5588c1862aee78258a89ed56795bfbb34
hang-on <hang-on@users.noreply.github.com>
2025-04-05 22:58:35 +0200

    Update session titles and clef assignments for consistency in sessionData

--- src/data/sessions.js
+++ src/data/sessions.js
@@ -14,21 +14,21 @@ export function getCurrentSession() {
 export const sessionData = [
     {
         value: "1",
-        title: "C Major Scale, F-Clef",
+        title: "C Major Scale, F-Clef, Set 1",
         clef: "F",
         key: "C",
         notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'b3', 'a3', 'b3', 'c4']
     },
     {
         value: "2",
-        title: "C Major Scale, G-Clef",
-        clef: "G",
+        title: "C Major Scale, F-Clef, Set 2",
+        clef: "F",
         key: "C",
-        notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'b3', 'a3', 'b3', 'c4']
+        notes: ['e2', 'f2', 'g2', 'a2']
     },
     {
         value: "3",
-        title: "A Major Scale, G-Clef",
+        title: "C Major Scale, G-Clef",
         clef: "G",
         key: "A",
         notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'b3', 'a3', 'b3', 'c4']
