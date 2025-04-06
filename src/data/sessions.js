46619112eddbce2613a389e1c77a7c22deaeb835
hang-on <hang-on@users.noreply.github.com>
2025-04-06 09:40:12 +0200

    Update session titles and notes for clarity and completeness

--- src/data/sessions.js
+++ src/data/sessions.js
@@ -14,30 +14,30 @@ export function getCurrentSession() {
 export const sessionData = [
     {
         value: "1",
-        title: "C Major Scale, F-Clef, Set 1",
+        title: "C Major Scale, F-Clef, Full Set",
         clef: "F",
         key: "C",
-        notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'b3', 'a3', 'b3', 'c4']
+        notes: ['e2', 'f2', 'g2', 'b2', 'c3', 'd3', 'e3', 'f3', 'g3', 'b3', 'a3', 'b3', 'c4']
     },
     {
         value: "2",
-        title: "C Major Scale, F-Clef, Set 2",
+        title: "C Major Scale, F-Clef, Low Set ",
         clef: "F",
         key: "C",
-        notes: ['e2', 'f2', 'g2', 'a2']
+        notes: ['e2', 'f2', 'g2', 'a2', 'b2', 'c3', 'd3', 'e3']
     },
     {
         value: "3",
-        title: "C Major Scale, G-Clef",
+        title: "C Major Scale, G-Clef, Full Set",
         clef: "G",
         key: "C",
-        notes: ['a5', 'f5', 'g5']
+        notes: ['c4', 'd4', 'e4', 'f4', 'g4', 'a4', 'b4', 'c5', 'd5', 'e5', 'f5', 'g5', 'a5']
     },
     {
         value: "4",
         title: "A Major Scale, G-Clef",
         clef: "G",
         key: "A",
-        notes: ['c#4', 'd4', 'e4', 'f#4', 'g#4', 'b4']
+        notes: ['c#4', 'd4', 'e4', 'f#4', 'g#4', 'b4', 'a4', 'c#5', 'd5', 'e5', 'f#5', 'g#5', 'a5']
     }    
 ];
